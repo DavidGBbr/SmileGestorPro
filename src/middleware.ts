ee6055fb6a52b92c74f1b0268aa382f02d4afcc1
davidGBbr <davidgomesbrigido@gmail.com>
2023-12-02 16:49:01 -0300

    protecting pages - pt3

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,21 +4,41 @@ import type { NextRequest } from "next/server";
 export async function middleware(req: NextRequest) {
   const token = req.cookies.get("@clinic.token")?.value;
 
-  const signInURL = new URL("/login", req.url);
-  const dashboardURL = new URL("/dashboard", req.url);
+  const routes = {
+    signInURL: new URL("/login", req.url),
+    dashboardURL: new URL("/dashboard", req.url),
+    proceduresURL: new URL("/procedures", req.url),
+    profileURL: new URL("/profile", req.url),
+  };
 
   if (token) {
-    if (req.nextUrl.pathname.startsWith("/login")) {
-      return NextResponse.redirect(dashboardURL);
+    if (
+      req.nextUrl.pathname.startsWith("/login") ||
+      req.nextUrl.pathname.startsWith("/register")
+    ) {
+      return NextResponse.redirect(routes.dashboardURL);
     }
   } else {
-    if (req.nextUrl.pathname.startsWith("/dashboard")) {
-      return NextResponse.redirect(signInURL);
+    if (
+      req.nextUrl.pathname.startsWith("/dashboard") ||
+      req.nextUrl.pathname.startsWith("/procedures") ||
+      req.nextUrl.pathname.startsWith("/profile")
+    ) {
+      return NextResponse.redirect(routes.signInURL);
     }
   }
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/dashboard", "/dashboard/:path*", "/login"],
+  matcher: [
+    "/dashboard",
+    "/dashboard/:path*",
+    "/procedures",
+    "/procedures/:path*",
+    "/profile",
+    "/profile/:path*",
+    "/login",
+    "/register",
+  ],
 };
