0932c3016df37be182abd0ac9ad5c511804d0a1b
davidGBbr <davidgomesbrigido@gmail.com>
2023-12-05 20:05:56 -0300

    registered service

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,14 +1,71 @@
 "use client";
 import { Sidebar } from "@/components/sidebar";
+import { setupAPIClient } from "@/services/api";
 import { Button, Flex, Heading, Input, Select } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 
+interface ProcedureProps {
+  id: string;
+  name: string;
+  price: string | number;
+  status: boolean;
+  user_id: string;
+}
+
 export default function New() {
   const [customer, setCustomer] = useState("");
+  const [procedures, setProcedures] = useState<ProcedureProps[]>([]);
+  const [procedureSelected, setProcedureSelected] = useState(
+    procedures.length && procedures[0]
+  );
+  const [datetime, setDatetime] = useState("");
 
   useEffect(() => {
     document.title = "Novo agendamento";
   }, []);
+
+  useEffect(() => {
+    const getProcedures = async () => {
+      const apiClient = setupAPIClient();
+      const response = await apiClient.get("/procedures", {
+        params: {
+          status: true,
+        },
+      });
+      setProcedures(response.data);
+
+      if (response.data === null) {
+        window.location.href = "/dashboard";
+      }
+    };
+
+    try {
+      getProcedures();
+    } catch (error) {
+      console.log(error);
+      window.location.href = "/dashboard";
+    }
+  }, []);
+
+  const handleChangeSelect = (id: string) => {
+    const procedureItem = procedures.find((procedure) => procedure.id === id);
+    setProcedureSelected(procedureItem);
+  };
+
+  const handleRegister = async () => {
+    try {
+      const apiClient = setupAPIClient();
+      const response = await apiClient.post("/schedule", {
+        customer: customer,
+        procedure_id: procedureSelected.id,
+        date: `${datetime}:00.000Z`,
+      });
+
+      window.location.href = "/dashboard";
+    } catch (error) {
+      alert("Erro ao registrar");
+    }
+  };
 
   return (
     <Sidebar>
@@ -42,7 +99,6 @@ export default function New() {
             onChange={(e) => setCustomer(e.target.value)}
           />
           <Input
-            placeholder="Selecione a data e horário para atendimento."
             w="85%"
             mb={3}
             size="lg"
@@ -55,11 +111,26 @@ export default function New() {
                   center/80% no-repeat;
               }
             `}
+            value={datetime}
+            onChange={(e) => setDatetime(e.target.value)}
           />
-          <Select bg="gray.900" color="#fff" mb={3} size="lg" w="85%">
-            <option key={1} value="Extração de siso">
-              Extração de siso
-            </option>
+          <Select
+            bg="gray.900"
+            color="#fff"
+            mb={3}
+            size="lg"
+            w="85%"
+            onChange={(e) => handleChangeSelect(e.target.value)}
+          >
+            {procedures?.map((procedure) => (
+              <option
+                key={procedure.id}
+                value={procedure.id}
+                style={{ color: "black" }}
+              >
+                {procedure.name}
+              </option>
+            ))}
           </Select>
 
           <Button
@@ -68,6 +139,7 @@ export default function New() {
             color="gray.900"
             bg="button.cta"
             _hover={{ bg: "#ffb13e" }}
+            onClick={handleRegister}
           >
             Cadastrar
           </Button>
